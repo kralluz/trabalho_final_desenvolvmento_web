5f00f57953a066d45293c5693618ab8e4622a0a8
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2025-04-21 11:35:11 -0300

    Create Connection.php

---
+++ app/Models/Connection.php
@@ -0,0 +1,24 @@
+<?php
+// Connection.php
+class Connection {
+    protected static function getConnection(): \PDO {
+        $dbFile = __DIR__ . '/database/database.sqlite';
+
+        if (!file_exists(dirname($dbFile))) {
+            mkdir(dirname($dbFile), 0755, true);
+        }
+
+        if (!file_exists($dbFile)) {
+            touch($dbFile);
+        }
+
+        $dsn = 'sqlite:' . $dbFile;
+
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        ];
+
+        return new \PDO($dsn, null, null, $options);
+    }
+}
